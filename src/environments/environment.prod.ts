b624e98b40ccff1ae6a4e13a5ac49c4b4c767e54
Mario Cardoza <mariokr.rocker@gmail.com>
2023-10-19 12:05:59 -0600

    [UPDATE] * enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  external: false,
+  urls: {
+    me: {
+      host: 'http://localhost',
+      port: '4200',
+      prefix: ''
+    },
+    api:{
+      host: 'https://localhost',
+      port: '7149',
+      prefix: 'api'
+    }
+  },
+  apiUrl: "http://locahost/4200"
 };
